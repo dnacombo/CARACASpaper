5b22f7ee2ea7592890e23c93d93fedf663a10835
Maximilien Chaumon <maximilien.chaumon@protonmail.com>
2025-04-30 18:41:01 +0200

    update

--- script_03_SDT.m
+++ script_03_SDT.m
@@ -5,8 +5,15 @@ setpath_ds003690
 load(fullfile(dirout,'AllFilesAndScoresList.mat'))
 
 
-%% 
+%%
 
+CORRrej = [];CARACASrej = [];SASICARACASrej = [];
+for i = 1:numel(fs)
+    CORRrej(i,:) = fs(i).CORR.rej;
+    CARACASrej(i,:) = fs(i).CARACAS.rej;
+    SASICARACASrej(i,:) = fs(i).SASICARACAS.rej;
+end
+%%
 figure(595);clf
 set(gcf,'UserData',fs)
 subplot(321)
@@ -14,16 +21,16 @@ hi = imagesc(CORRrej');
 title('CORR')
 set(hi,"ButtonDownFcn",@plotit);
 subplot(322);
-hi = imagesc(CARArej');
+hi = imagesc(CARACASrej');
 title('CARACAS')
 set(hi,"ButtonDownFcn",@plotit);
 
 subplot(3,2,[3 6]);
 toplot = NaN(size(CORRrej));
-toplot(CORRrej & CARArej) = 1;
-toplot(CORRrej & ~ CARArej) = 2;
-toplot(~CORRrej & ~CARArej) = 3;
-toplot(~CORRrej & CARArej) = 4;
+toplot(CORRrej & CARACASrej) = 1;
+toplot(CORRrej & ~ CARACASrej) = 2;
+toplot(~CORRrej & ~CARACASrej) = 3;
+toplot(~CORRrej & CARACASrej) = 4;
 names = {'Hit','Miss','CR','FA'};
 
 hi = imagesc(toplot');
@@ -65,30 +72,38 @@ plot(comp.time{1},comp.trial{1}(icmp,:))
 
 drawnow
 
-cfg = [];
+cfgtmp = [];
+if isfield(comp, 'sampleinfo')
+    cfgtmp.trl = [1, comp.sampleinfo(end,2), 0];
+else
+    cfgtmp.trl = [1, sum(cellfun(@numel,comp.time)), 0]; % Start, End, Offset
+end
+comp = ft_redefinetrial(cfgtmp, comp);
 
-comp.trial = cellfun(@(x) x(icmp,:),comp.trial, 'UniformOutput',false);
+comp.trial{1} = comp.trial{1}(icmp,:);
 comp.label = comp.label(icmp);
 comp.topolabel = comp.topolabel(icmp);
 
-    cfg = [];
-    cfg.method_chosen = 'absolute_threshold';   %'absolute_threshold' (method 1) or 'mean_std' (method 2)
-    cfg.plot_heart_IC = 0;                      % 1 or 0 (to plot the IC labelled as cardiac)
-    cfg.path_output = []; % path where you want to save i) the distribution of the scores (among all IC) and ii) timecourse of the identified cardiac IC (used only if plot_heart_IC == 1)
-    cfg.file_info = myfileparts(fs(ids).name,'f');% name of your recording (to add it in the name of your plot files) (used only if plot_heart_IC == 1)
-    cfg.nb_IC_wanted = 3;                       % number of IC selected for each metric (kurtosis, skewness...) [default: 3, to select the top 3 IC for each metric]
-    cfg.bpm_min = 45;                           % expected heart beat per min, for sanity check [default: 45 and 90]
-    cfg.bpm_max = 90;
-    cfg.threshold_cond_IC_method1 = .5;         % minimum proportion of conditions that must be met in order that an IC could be considered as a potential heart IC [default: 0.5, so if method_chosen == 'absolute_threshold', an IC must be in the top 3 for at least 50% of the metrics]
-    cfg.threshold_std_method2 = 2.5;            % if method_chosen == 'mean_std', an IC will be considered as a potential heart IC if its proportion of conditions met (i.e., its score) is above mean(all_score) + threshold_std_method2 * std(all_score) [default: 2.5]
-    cfg.min_recording_duration_sec = 20;        % minimum duration (in sec) of the IC timecourse (default: 20]
-    cfg.mini_bouts_duration_for_SignalAmplRange = 10; % for sanity check (avoids false positive): the time course of a potential heart IC must be ~regular. The timecourse will be divided into mini-segments of this duration, and we will check that the amplitude between these mini-bouts is ~similar. [default: 10]
-    cfg.threshold_regularity_signal_minmax = 1.5; % For each mini-bout, the averaged signal amplitude is computed. The IC timecourse will be considered as irregular if: (max(Mean_Amp_minibout) - min(Mean_Amp_minibout)) / min(Mean_Amp_minibout) > threshold_regularity_signal_minmax [default: 1.5]
+cfg = [];
+cfg.method_chosen = 'absolute_threshold';   %'absolute_threshold' (method 1) or 'mean_std' (method 2)
+cfg.plot_heart_IC = 0;                      % 1 or 0 (to plot the IC labelled as cardiac)
+cfg.path_output = []; % path where you want to save i) the distribution of the scores (among all IC) and ii) timecourse of the identified cardiac IC (used only if plot_heart_IC == 1)
+cfg.file_info = myfileparts(fs(ids).name,'f');% name of your recording (to add it in the name of your plot files) (used only if plot_heart_IC == 1)
+cfg.nb_IC_wanted = 3;                       % number of IC selected for each metric (kurtosis, skewness...) [default: 3, to select the top 3 IC for each metric]
+cfg.bpm_min = 45;                           % expected heart beat per min, for sanity check [default: 45 and 90]
+cfg.bpm_max = 90;
+cfg.threshold_cond_IC_method1 = .5;         % minimum proportion of conditions that must be met in order that an IC could be considered as a potential heart IC [default: 0.5, so if method_chosen == 'absolute_threshold', an IC must be in the top 3 for at least 50% of the metrics]
+cfg.threshold_std_method2 = 2.5;            % if method_chosen == 'mean_std', an IC will be considered as a potential heart IC if its proportion of conditions met (i.e., its score) is above mean(all_score) + threshold_std_method2 * std(all_score) [default: 2.5]
+cfg.min_recording_duration_sec = 20;        % minimum duration (in sec) of the IC timecourse (default: 20]
+cfg.mini_bouts_duration_for_SignalAmplRange = 10; % for sanity check (avoids false positive): the time course of a potential heart IC must be ~regular. The timecourse will be divided into mini-segments of this duration, and we will check that the amplitude between these mini-bouts is ~similar. [default: 10]
+cfg.threshold_regularity_signal_minmax = 1.5; % For each mini-bout, the averaged signal amplitude is computed. The IC timecourse will be considered as irregular if: (max(Mean_Amp_minibout) - min(Mean_Amp_minibout)) / min(Mean_Amp_minibout) > threshold_regularity_signal_minmax [default: 1.5]
 
-    [tmp, a, b, c] = CARACAS(cfg, comp)
+[isCardiac, meas] = CARACAS(cfg, comp);
 
 corr = fs(ids).CORR.c(icmp)
 
+meas
+isCardiac
 % cfg = [];
 % cfg.viewmode  = 'component';
 % cfg.layout    = layout;
--- script_04_print_comps.m
+++ script_04_print_comps.m
@@ -1,5 +1,7 @@
 setpath_ds003690
 
+dont_plot_topos = 0;
+
 mymkdir(fullfile(dirout,'AllCompImages'))
 
 load(fullfile(dirout,'AllFilesAndScoresList.mat'))
@@ -13,34 +15,43 @@ s = orderfields(s, {'id','sub','task','run'});
 writetable(struct2table(s),'allfiles.csv');
 
 %% 
-figure(4048);
-set(gcf,'WindowState','maximized')
+
 
 for i_f = 1:numel(fs)
-    clf
-    outpng = fullfile(dirout,'AllCompImages', fs(i_f).mod,[fs(i_f).sub,'_', fs(i_f).task,'_', fs(i_f).run, '_allComps.png']);
+    outpng = fullfile(dirout,'AllCompImages',[fs(i_f).sub,'_', fs(i_f).task,'_', fs(i_f).run, '_allComps_',ifelse(dont_plot_topos,'tc','topo'), '.png']);
     fprintf('%s\n',myfileparts(outpng,'f'));
     png = dir(outpng);
     compmat = dir(fs(i_f).compf);
     if isempty(png) || png.datenum < compmat.datenum
+        figure('position', [ 1         121        1920         988]);
+        set(gcf,'WindowState','maximized')
+
         comp = load(fs(i_f).compf);
+        comp.cfg = [];
         %%
         for i_c = 1:numel(comp.label)
-    
-            h = subplot(12,10,i_c+10*(floor((i_c-1)/10)));
-            cfg = [];
-            cfg.layout = layout;
-            cfg.component = i_c;
-            cfg.channel = 'eeg';
-            cfg.figure = h;
-            cfg.comment = 'no';
-            cfg.checkstring = 'no';
-            ft_topoplotIC(cfg,comp)
-    
-            h = subplot(12,10,i_c+10*(floor((i_c-1)/10))+10);
-            plot(comp.time{1},comp.trial{1}(i_c,:))
+            if dont_plot_topos
+
+                h = subplot(6,10,i_c);
+                plot(comp.time{1},comp.trial{1}(i_c,:))
+                title(sprintf('Component %d',i_c))
+            else
+                h = subplot(12,10,i_c+10*(floor((i_c-1)/10)));
+                cfg = [];
+                cfg.layout = layout;
+                cfg.component = i_c;
+                cfg.channel = 'eeg';
+                cfg.figure = h;
+                cfg.comment = 'no';
+                cfg.markersymbol = '.';
+                ft_topoplotIC(cfg,comp)
+
+                h = subplot(12,10,i_c+10*(floor((i_c-1)/10))+10);
+                plot(comp.time{1},comp.trial{1}(i_c,:))
+            end
         end
         drawnow
         saveas(gcf,outpng)
+        close(gcf)
     end
 end
