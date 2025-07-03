4bcb19b8326bcf451c8edd451d80e149d4b8af5a
Maximilien Chaumon <maximilien.chaumon@protonmail.com>
2025-07-03 17:44:10 +0200

    update

--- script_03_collect_fileslist.m
+++ script_03_collect_fileslist.m
@@ -1,4 +1,6 @@
 %%
+which_data = '_SASICA'; % '' // '_30comp'
+
 setpath_ds003690
 %% collect all files created by script_02_ds003690_precomp
 
@@ -11,19 +13,21 @@ for i_f = 1:numel(fs)
         error('mixing things up...')
     end
     ffs(i_f) = removefields(f,'MANUAL');
+    ffs(i_f).CARACAS.meas = removefields(ffs(i_f).CARACAS.meas,{'QS', 'ST', 'PR', 'RT', 'PT', 'Ampl_var'});
 end
 fs = ffs;
 save(fullfile(dirout,'AllFilesAndScoresList.mat'), 'fs');
 %% collect all ratings done by Pierre
 load(fullfile(dirout,'AllFilesAndScoresList.mat'), 'fs');
 
-Pierre = readtext('/network/iss/cenir/analyse/meeg/CARACAS/allfiles_ds3690_verif_Pierre.csv', ',');
-Pierre = Pierre(:,1:5);
+Pierre = readtext(['/network/iss/cenir/analyse/meeg/CARACAS/allfiles_ds3690_verif_Pierre' which_data '.csv'], ';', '','"');
+Pierre = Pierre(:,1:7);
 Paul = struct();
 for i = 2:size(Pierre,1)
     for j = 1:size(Pierre,2)
         Paul(i-1).(Pierre{1,j}) = Pierre{i,j};
-        if j == 5 && ischar(Paul(i-1).(Pierre{1,j}))
+        if ismember(j, [5 6 7]) && ischar(Paul(i-1).(Pierre{1,j}))
+            Paul(i-1).(Pierre{1,j}) = strrep(Paul(i-1).(Pierre{1,j}), '"','');
             Paul(i-1).(Pierre{1,j}) = str2double(strsplit(Paul(i-1).(Pierre{1,j}),';'));
         end
     end
@@ -37,7 +41,11 @@ for i_f = 1:numel(fs)
         error('Ambiguous')
     end
     fs(i_f).MANUAL.rej = false(size(fs(i_f).CARACAS.rej));
+    fs(i_f).MANUAL.rej_sure = false(size(fs(i_f).CARACAS.rej));
+    fs(i_f).MANUAL.rej_noisy = false(size(fs(i_f).CARACAS.rej));
     fs(i_f).MANUAL.rej(P.CardiComps) = 1;
+    fs(i_f).MANUAL.rej_sure(P.CardiComps_good) = 1;
+    fs(i_f).MANUAL.rej_noisy(P.CardiComps_noisy) = 1;
 end
 
 save(fullfile(dirout,'AllFilesAndScoresList.mat'), 'fs');
